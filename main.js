3f3530125bf94dd7bfdf3da30cb6ead41008f187
Aadhishankaran <78032579+Aadhishankaran@users.noreply.github.com>
2022-07-12 16:35:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,30 @@
-img = "";
+video = "";
 status = "";
-objects = [];
 
 function preload(){
-    img = loadImage('dog_cat.jpg')
-}
-
-function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380,380);
+    video = createVideo('video.mp4')
     video.hide();
 }
 
-function start(){
-    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Object Detected"
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
 }
 
 function draw() {
-    image(video, 0, 0, 380, 380)
-
-        if(status != "")
-        {
-            for (i = 0; i < objects.length; i++)
-            {
-                r = random(255)
-                b = random(255)
-                g = random(255)
-                document.getElementById("status").innerHTML = "Status : Object Detected";
-                document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
-                fill(r,g,b)
-                percent = floor(objects[i].confidence * 100)
-                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
-                noFill();
-                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-            }
-        }
+    image(video, 0, 0, 480, 380);
 }
+
+function start()
+    {
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+    }
 
 function modelLoaded() {
-    console.log("Model Loaded!")
-    status = true;
-    objectDetector.detect(video, gotResult);
-}
-
-function gotResult(error, results) {
-    if (error) {
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
+    console.log("Model Loaded!");
+    status = true; 
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
